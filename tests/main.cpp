fa63002babba4b13a0bbff2a0026c058c8dc9478
Aleksandr Tretyakov <flonednb@gmail.com>
2022-03-02 20:39:50 +0500

    Add memory checks for tests.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include "Catch2/catch.hpp"
 
-int main() { Catch::Session().run(); }
+#ifndef DEBUG
+#include <Windows.h>
+#endif
+
+int main() {
+    // Enable run-time memory check for debug builds.
+#if defined(DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#else
+    OutputDebugStringA("Using release build configuration.");
+#endif
+    Catch::Session().run();
+}

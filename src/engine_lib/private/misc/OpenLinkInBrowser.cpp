5093aadf9bdd96f6b63f28c20c3737a56f63f207
Aleksandr Tretyakov <flonednb@gmail.com>
2022-06-18 22:46:21 +0500

    Add OpenLinkInBrowser.

---
+++ src/engine_lib/private/misc/OpenLinkInBrowser.cpp
@@ -0,0 +1,18 @@
+﻿#include "misc/OpenLinkInBrowser.h"
+
+#if defined(WIN32)
+#include <Windows.h>
+#undef MessageBox
+#undef IGNORE
+#endif
+
+namespace ne {
+    void OpenLinkInBrowser::open(const std::string& sLink) {
+#if defined(WIN32)
+        ShellExecuteA(nullptr, "open", sLink.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
+#elif __linux__
+        static_assert(false, "check if this part actually works");
+        system((std::string("open ") + sLink).c_str());
+#endif
+    }
+} // namespace ne
---
+++ src/engine_lib/public/misc/OpenLinkInBrowser.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+// STL.
+#include <string>
+
+namespace ne {
+    /** Simple class that provides a static function to open a link in default browser. */
+    class OpenLinkInBrowser {
+    public:
+        /**
+         * Opens a web link in user's default browser.
+         *
+         * @param sLink Web link. Example: "https://github.com/Flone-dnb/nameless-engine".
+         */
+        static void open(const std::string& sLink);
+    };
+} // namespace ne

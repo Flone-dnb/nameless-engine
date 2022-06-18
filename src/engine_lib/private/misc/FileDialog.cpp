fb89c773c2578698339d077142ca85a0b923fa33
Aleksandr Tretyakov <flonednb@gmail.com>
2022-06-18 23:24:34 +0500

    Add FileDialog.

---
+++ src/engine_lib/private/misc/FileDialog.cpp
@@ -0,0 +1,44 @@
+﻿#include "misc/FileDialog.h"
+
+// External.
+#include "portable-file-dialogs.h"
+#if defined(WIN32)
+#pragma push_macro("MessageBox")
+#undef MessageBox
+#pragma push_macro("IGNORE")
+#undef IGNORE
+#endif
+
+namespace ne {
+    std::vector<std::filesystem::path> FileDialog::openFile(
+        const std::string& sTitle,
+        const std::vector<std::pair<std::string, std::string>>& vFileTypeFilters,
+        const std::filesystem::path& directory,
+        bool bAllowSelectingMultipleFiles) {
+        std::vector<std::string> vFilters;
+
+        for (const auto& [sName, sTypeFilters] : vFileTypeFilters) {
+            vFilters.push_back(std::format("{} ({})", sName, sTypeFilters));
+            vFilters.push_back(sTypeFilters);
+        }
+
+        const auto vResult = pfd::open_file(
+                                 sTitle,
+                                 directory.string(),
+                                 vFilters,
+                                 bAllowSelectingMultipleFiles ? pfd::opt::multiselect : pfd::opt::none)
+                                 .result();
+        // Convert result.
+        std::vector<std::filesystem::path> vSelectedPaths;
+        for (const auto& sPath : vResult) {
+            vSelectedPaths.push_back(sPath);
+        }
+
+        return vSelectedPaths;
+    }
+} // namespace ne
+
+#if defined(WIN32)
+#pragma pop_macro("IGNORE")
+#pragma pop_macro("MessageBox")
+#endif
---
+++ src/engine_lib/public/misc/FileDialog.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+// STL.
+#include <filesystem>
+#include <vector>
+#include <string>
+
+namespace ne {
+    /** Various file dialog windows: open file, save file, select directory and etc. */
+    class FileDialog {
+    public:
+        /**
+         * Shows "Open File" dialog window.
+         *
+         * Example:
+         * @code
+         * const auto vSelected = ne::FileDialog::openFile(
+         *     "Select audio and image files",
+         *     {
+         *       {"Audio Files", "*.mp3 *.wav *.flac *.ogg"},
+         *       {"Image Files", "*.png *.jpg *.jpeg *.bmp"}
+         *     },
+         *     std::filesystem::current_path(),
+         *     true);
+         * @endcode
+         *
+         * @param sTitle                        Title of the dialog window.
+         * @param vFileTypeFilters              Filter that determines which types of files can be selected.
+         * Each pair contains filter name and space separated types.
+         * @param directory                     Starting directory to show.
+         * @param bAllowSelectingMultipleFiles  Whether to allow selecting multiple files or not.
+         *
+         * @return Array of selected files (if bAllowSelectingMultipleFiles is 'true'), one selected file
+         * (if bAllowSelectingMultipleFiles is 'false') and empty array if the used clicked "Cancel".
+         */
+        static std::vector<std::filesystem::path> openFile(
+            const std::string& sTitle,
+            const std::vector<std::pair<std::string, std::string>>& vFileTypeFilters = {{"All Files", {"*"}}},
+            const std::filesystem::path& directory = std::filesystem::current_path(),
+            bool bAllowSelectingMultipleFiles = false);
+    };
+} // namespace ne

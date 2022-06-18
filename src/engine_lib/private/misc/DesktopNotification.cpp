a71915e70cd59e24badf1d844fb3c022d3b79d13
Aleksandr Tretyakov <flonednb@gmail.com>
2022-06-18 22:29:10 +0500

    Add DesktopNotification.

---
+++ src/engine_lib/private/misc/DesktopNotification.cpp
@@ -0,0 +1,18 @@
+﻿#include "misc/DesktopNotification.h"
+
+// External.
+#include "portable-file-dialogs.h"
+
+namespace ne {
+    void DesktopNotification::info(const std::string& sTitle, const std::string& sText) {
+        pfd::notify(sTitle, sText, pfd::icon::info);
+    }
+
+    void DesktopNotification::warning(const std::string& sTitle, const std::string& sText) {
+        pfd::notify(sTitle, sText, pfd::icon::warning);
+    }
+
+    void DesktopNotification::error(const std::string& sTitle, const std::string& sText) {
+        pfd::notify(sTitle, sText, pfd::icon::error);
+    }
+} // namespace ne
---
+++ src/engine_lib/public/misc/DesktopNotification.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+// STL.
+#include <string>
+
+namespace ne {
+    /** Show desktop notifications (popups). */
+    class DesktopNotification {
+    public:
+        /**
+         * Show information notification.
+         *
+         * @param sTitle  Notification title.
+         * @param sText   Notification text (content).
+         */
+        static void info(const std::string& sTitle, const std::string& sText);
+
+        /**
+         * Show warning notification.
+         *
+         * @param sTitle  Notification title.
+         * @param sText   Notification text (content).
+         */
+        static void warning(const std::string& sTitle, const std::string& sText);
+
+        /**
+         * Show error notification.
+         *
+         * @param sTitle  Notification title.
+         * @param sText   Notification text (content).
+         */
+        static void error(const std::string& sTitle, const std::string& sText);
+    };
+} // namespace ne

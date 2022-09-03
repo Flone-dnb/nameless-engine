6a47d978c921aa10bed7d60caf1910b213989ae9
Aleksandr Tretyakov <flonednb@gmail.com>
2022-09-03 19:03:21 +0500

    Wrap DontSerializeProperty into ne namespace.

--- src/engine_lib/public/io/DontSerializeProperty.h
+++ src/engine_lib/public/io/DontSerializeProperty.h
@@ -5,17 +5,23 @@
 
 #include "DontSerializeProperty.generated.h"
 
-/**
- * Add this property to your reflected field to ignore it when serializing an object.
- *
- * Example:
- * @code
- * NEPROPERTY(DontSerialize)
- * int iKey = 42; // will be ignored and not serialized
- * @endcode
- */
-class NECLASS(rfk::PropertySettings(rfk::EEntityKind::Field, false, true)) DontSerialize
-    : public rfk::Property {
-public:
-    DontSerialize_GENERATED
-};
+namespace ne NENAMESPACE() {
+    /**
+     * Add this property to your reflected field to ignore it when serializing an object.
+     *
+     * Example:
+     * @code
+     * // #include here
+     * // ...
+     * NEPROPERTY(ne::DontSerialize)
+     * int iKey = 42; // will be ignored and not serialized
+     * @endcode
+     */
+    class NECLASS(rfk::PropertySettings(rfk::EEntityKind::Field, false, true)) DontSerialize
+        : public rfk::Property {
+    public:
+        ne_DontSerialize_GENERATED
+    };
+} // namespace ne NENAMESPACE()
+
+File_DontSerializeProperty_GENERATED

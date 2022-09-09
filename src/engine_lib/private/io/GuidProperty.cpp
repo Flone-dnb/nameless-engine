f78c57d1bdbbe229e12c884859b71b6b2950402c
Aleksandr Tretyakov <flonednb@gmail.com>
2022-09-09 23:39:21 +0500

    Add `Guid` property for classes and structs.
    In this commit I add `Guid` property to specify GUID for reflected
    classes and structs. We will use this GUID in
    serialization/deserialization instead of the class ID since class ID
    is not really stable.

---
+++ src/engine_lib/private/io/GuidProperty.cpp
@@ -0,0 +1,37 @@
+﻿#include "io/GuidProperty.h"
+
+// Custom.
+#include "misc/Error.h"
+
+// External.
+#include "fmt/format.h"
+
+namespace ne {
+    Guid::Guid(const char* pGuid) : sGuid(pGuid) {
+#if DEBUG
+        const std::string sGuidInformation =
+            "Here is an example GUID: \"00000000-0000-0000-0000-000000000000\"\n"
+            "You can generate a random GUID by just googling \"generate GUID\".";
+
+        // Check GUID length.
+        if (sGuid.size() != 36) {
+            const Error err(fmt::format(
+                "The specified GUID \"{}\" should have a length of 36 characters.\n{}",
+                sGuid,
+                sGuidInformation));
+            err.showError();
+            throw std::runtime_error(err.getError());
+        }
+
+        // Check GUID format.
+        if (sGuid[8] != '-' || sGuid[13] != '-' || sGuid[18] != '-' || sGuid[23] != '-') {
+            const Error err(
+                fmt::format("The specified GUID \"{}\" has incorrect format.\n{}", sGuid, sGuidInformation));
+            err.showError();
+            throw std::runtime_error(err.getError());
+        }
+#endif
+    }
+
+    std::string Guid::getGuid() const { return sGuid; }
+} // namespace ne
---
+++ src/engine_lib/public/io/GuidProperty.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+// STL.
+#include <string>
+#include <string_view>
+
+// External.
+#include "Refureku/Properties/PropertySettings.h"
+
+#include "GuidProperty.generated.h"
+
+namespace ne NENAMESPACE() {
+    /**
+     * GUID property for classes and structs that inherit Serializable class.
+     * This property is required for all derived classes/structs of Serializable class.
+     * GUID defined a unique identifier for your class/struct and is used in serialization/deserialization.
+     *
+     * Usage example:
+     * @code
+     * class NECLASS(ne::Guid("00000000-0000-0000-0000-000000000000")) MyCoolClass : public ne::Serializable
+     * @endcode
+     *
+     * You can generate a random GUID by just googling "generate GUID". Fun fact: if you search
+     * "generate GUID" in DuckDuckGo it will show you a randomly generated GUID on top of all search results.
+     *
+     * Uniqueness of all GUIDs is checked by the engine on startup in DEBUG builds, so you don't need to check
+     * if all of your GUIDs are unique or not, this is done automatically and if something is not unique
+     * you will get a message box with an error on engine startup saying where and what is not unique.
+     */
+    class NECLASS(rfk::PropertySettings(rfk::EEntityKind::Class | rfk::EEntityKind::Struct, false, true)) Guid
+        : public rfk::Property {
+    public:
+        /**
+         * Initializes the GUID of the entity.
+         *
+         * @param pGuid GUID of the entity.
+         */
+        Guid(const char* pGuid);
+
+        /**
+         * Returns entity's GUID.
+         *
+         * @return Entity's GUID.
+         */
+        std::string getGuid() const;
+
+    private:
+        /** Entity's GUID. */
+        std::string sGuid;
+
+        ne_Guid_GENERATED
+    };
+} // namespace )
+
+File_GuidProperty_GENERATED

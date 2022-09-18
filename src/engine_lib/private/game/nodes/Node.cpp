0518186416995704803d18823c15ae2b20cb9645
Aleksandr Tretyakov <flonednb@gmail.com>
2022-09-18 18:04:43 +0500

    Implement basic `Node` functionality.

--- src/engine_lib/private/game/nodes/Node.cpp
+++ src/engine_lib/private/game/nodes/Node.cpp
@@ -1,11 +1,207 @@
 ﻿#include "game/nodes/Node.h"
 
-namespace ne {
-    Node::Node() { sName = "Node"; }
+// Custom.
+#include "io/Logger.h"
 
-    Node::Node(const std::string& sName) { this->sName = sName; }
+#if defined(DEBUG)
+/** Total amount of created nodes. */
+static std::atomic<size_t> iTotalNodeCount{0};
+#endif
+
+namespace ne {
+    Node::Node() : Node("Node") {}
+
+    Node::Node(const std::string& sName) {
+        this->sName = sName;
+        mtxParentNode.second = nullptr;
+        mtxIsSpawned.second = false;
+
+#if defined(DEBUG)
+        const size_t iNodeCount = iTotalNodeCount.fetch_add(1);
+        Logger::get().info(
+            fmt::format(
+                "[DEBUG ONLY] constructor for node \"{}\" is called (alive nodes now: {})",
+                sName,
+                iNodeCount),
+            sNodeLogCategory);
+#endif
+    }
+
+    Node::~Node() {
+#if defined(DEBUG)
+        const size_t iNodesLeft = iTotalNodeCount.fetch_sub(1);
+        Logger::get().info(
+            fmt::format(
+                "[DEBUG ONLY] destructor for node \"{}\" is called (alive nodes left: {})",
+                sName,
+                iNodesLeft),
+            sNodeLogCategory);
+#endif
+    }
 
     void Node::setName(const std::string& sName) { this->sName = sName; }
 
     std::string Node::getName() const { return sName; }
+
+    void Node::addChildNode(std::shared_ptr<Node> pNode) {
+        // Check if this node is valid.
+        if (pNode == nullptr) [[unlikely]] {
+            Logger::get().warn(
+                fmt::format(
+                    "an attempt was made to attach a nullptr node to the \"{}\" node, aborting this "
+                    "operation",
+                    getName()),
+                sNodeLogCategory);
+            return;
+        }
+
+        // Check if this node is this.
+        if (pNode.get() == this) [[unlikely]] {
+            Logger::get().warn(
+                fmt::format(
+                    "an attempt was made to attach the \"{}\" node to itself, aborting this "
+                    "operation",
+                    getName()),
+                sNodeLogCategory);
+            return;
+        }
+
+        std::scoped_lock guard(mtxChildNodes.first);
+
+        // Check if this node is already our child.
+        for (const auto& pChildNode : mtxChildNodes.second) {
+            if (pChildNode == pNode) [[unlikely]] {
+                Logger::get().warn(
+                    fmt::format(
+                        "an attempt was made to attach the \"{}\" node to the \"{}\" node but it's already "
+                        "a child node of \"{}\", aborting this operation",
+                        pNode->getName(),
+                        getName(),
+                        getName()),
+                    sNodeLogCategory);
+                return;
+            }
+        }
+
+        // Check if this node is already attached to some node.
+        {
+            std::scoped_lock parentGuard(pNode->mtxParentNode.first);
+            if (pNode->mtxParentNode.second != nullptr) {
+                // Change node parent.
+                pNode->onBeforeDetachedFromNode(pNode->mtxParentNode.second);
+
+                pNode->mtxParentNode.second = this;
+                mtxChildNodes.second.push_back(pNode);
+
+                pNode->onAfterAttachedToNode(mtxParentNode.second);
+            }
+        }
+
+        // Spawn/despawn node if needed.
+        if (isSpawned() && !pNode->isSpawned()) {
+            // Spawn node.
+            pNode->spawn();
+        } else if (!isSpawned() && pNode->isSpawned()) {
+            // Despawn node.
+            pNode->despawn();
+        }
+    }
+
+    void Node::detachFromParentAndDespawn() {
+        // Check if this node is spawned.
+        std::scoped_lock spawnGuard(mtxIsSpawned.first);
+        if (mtxIsSpawned.second == false)
+            return;
+
+        // Detach from parent.
+        std::scoped_lock parentGuard(mtxParentNode.first);
+        std::shared_ptr<Node> pSelf;
+
+        if (mtxParentNode.second != nullptr) {
+            // Remove this node from parent's children array.
+            std::scoped_lock parentChildGuard(mtxParentNode.second->mtxChildNodes.first);
+            auto pParentChildren = &mtxParentNode.second->mtxChildNodes.second;
+            for (auto it = pParentChildren->begin(); it != pParentChildren->end(); ++it) {
+                if (it->get() == this) {
+                    pSelf = *it; // keep a shared pointer to self until we are not finished
+                    pParentChildren->erase(it);
+                    break;
+                }
+            }
+
+            // Remove parent.
+            mtxParentNode.second = nullptr;
+        }
+
+        // Despawn.
+        despawn();
+    }
+
+    bool Node::isSpawned() {
+        std::scoped_lock guard(mtxIsSpawned.first);
+        return mtxIsSpawned.second;
+    }
+
+    void Node::spawn() {
+        std::scoped_lock guard(mtxIsSpawned.first);
+
+        if (mtxIsSpawned.second) [[unlikely]] {
+            Logger::get().warn(
+                fmt::format(
+                    "an attempt was made to spawn already spawned node \"{}\", aborting this operation",
+                    getName()),
+                sNodeLogCategory);
+            return;
+        }
+
+        // Spawn children.
+        {
+            std::scoped_lock childGuard(mtxChildNodes.first);
+
+            for (const auto& pChildNode : mtxChildNodes.second) {
+                pChildNode->spawn();
+            }
+        }
+
+        // Spawn self.
+        mtxIsSpawned.second = true;
+        onSpawn();
+    }
+
+    void Node::despawn() {
+        std::scoped_lock guard(mtxIsSpawned.first);
+
+        if (!mtxIsSpawned.second) [[unlikely]] {
+            Logger::get().warn(
+                fmt::format(
+                    "an attempt was made to despawn already despawned node \"{}\", aborting this operation",
+                    getName()),
+                sNodeLogCategory);
+            return;
+        }
+
+        // Despawn self.
+        onDespawn();
+        mtxIsSpawned.second = false;
+
+        // Despawn children.
+        {
+            std::scoped_lock childGuard(mtxChildNodes.first);
+
+            for (const auto& pChildNode : mtxChildNodes.second) {
+                pChildNode->despawn();
+            }
+        }
+    }
+
+    Node* Node::getParent() {
+        std::scoped_lock guard(mtxParentNode.first);
+        return mtxParentNode.second;
+    }
+
+    std::vector<std::shared_ptr<Node>> Node::getChildNodes() {
+        std::scoped_lock guard(mtxChildNodes.first);
+        return mtxChildNodes.second;
+    }
+
 } // namespace ne
--- src/engine_lib/public/game/nodes/Node.h
+++ src/engine_lib/public/game/nodes/Node.h
@@ -1,5 +1,11 @@
 ﻿#pragma once
 
+// STL.
+#include <vector>
+#include <memory>
+#include <mutex>
+#include <atomic>
+
 // Custom.
 #include "io/Serializable.h"
 
@@ -7,7 +13,8 @@
 
 namespace ne NENAMESPACE() {
     /**
-     * Base Node class, allows attaching child nodes or being attached to some parent node.
+     * Base class for nodes, allows being spawned in the world, attaching child nodes
+     * or being attached to some parent node.
      *
      * @warning If changing the class name class ID (of this class) will change and will make all previously
      * serialized instances of this class reference old (invalid) class ID. Include a backwards compatibility
@@ -25,7 +32,13 @@ namespace ne NENAMESPACE() {
          */
         Node(const std::string& sNodeName);
 
-        virtual ~Node() override = default;
+        Node(const Node&) = delete;
+        Node& operator=(const Node&) = delete;
+        Node(Node&&) = delete;
+        Node& operator=(Node&&) = delete;
+
+        /** Logs destruction in debug builds. */
+        virtual ~Node() override;
 
         /**
          * Sets node's name.
@@ -43,10 +56,125 @@ namespace ne NENAMESPACE() {
         NEFUNCTION()
         std::string getName() const;
 
+        /**
+         * Returns whether this node is spawned in the world or not.
+         *
+         * @return Whether this node is spawned in the world or not.
+         */
+        NEFUNCTION()
+        bool isSpawned();
+
+        /**
+         * Returns parent node if this node.
+         *
+         * @return Do not delete returned pointer.
+         * nullptr if there is no parent node, otherwise valid pointer.
+         */
+        NEFUNCTION()
+        Node* getParent();
+
+        /**
+         * Detaches this node from the parent and despawns this node and
+         * all of its child nodes.
+         *
+         * The node and its child nodes are not guaranteed to be deleted after this
+         * function is finished. Deletion is handled automatically by shared pointers
+         * (when nobody will reference a node). If you consider the node as no longer needed
+         * you can clear any shared pointers to this node that you hold. Once the world
+         * is changed (world's root node changed) old root node will be deleted and so
+         * references to all nodes in the world should be automatically cleaned up which
+         * should cause all nodes to be deleted, unless you are holding a shared pointer to a
+         * node outside of the Node class (in IGameInstance for example or in some other class).
+         */
+        NEFUNCTION()
+        void detachFromParentAndDespawn();
+
+        /**
+         * Returns a copy of the array of child nodes.
+         *
+         * @return A copy of the array of child nodes.
+         */
+        std::vector<std::shared_ptr<Node>> getChildNodes();
+
+        /**
+         * Attaches a node as a child of this node.
+         *
+         * @remark If attached node had a parent it will be changed.
+         *
+         * @param pNode Node to attach as a child.
+         */
+        void addChildNode(std::shared_ptr<Node> pNode);
+
+    protected:
+        /**
+         * Called right before this node will be detached from its current parent node.
+         *
+         * @remark Not called if node has no parent.
+         *
+         * @param pDetachingFromThis Current parent node that we will be detached from.
+         *
+         * @warning It's better to call parent's version first (before executing your logic).
+         */
+        virtual void onBeforeDetachedFromNode(Node* pDetachingFromThis){};
+
+        /**
+         * Called right after this node was attached to a new parent node.
+         *
+         * @param pAttachedToThis New parent node that we are attached to.
+         *
+         * @warning It's better to call parent's version first (before executing your logic).
+         */
+        virtual void onAfterAttachedToNode(Node* pAttachedToThis){};
+
+        /**
+         * Called when this node was not spawned and was attached to a parent node that is spawned
+         * to execute custom spawn logic.
+         *
+         * @remark This node will be marked as spawned before this function is called.
+         * @remark This function is called only after all child nodes were spawned.
+         *
+         * @warning It's better to call parent's version first (before executing your logic).
+         */
+        virtual void onSpawn(){};
+
+        /**
+         * Called before this node is despawned from the world to execute custom despawn logic.
+         *
+         * @remark This node will be marked as despawned after this function is called.
+         * @remark This function is called before child nodes are despawned.
+         *
+         * @warning It's better to call parent's version first (before executing your logic).
+         */
+        virtual void onDespawn(){};
+
     private:
+        /** Calls @ref onSpawn on this node and all of its child nodes. */
+        void spawn();
+
+        /** Calls @ref onDespawn on this node and all of its child nodes. */
+        void despawn();
+
         /** Node name. */
         NEPROPERTY()
         std::string sName;
+
+        /** Attached child nodes. Should be used under the mutex when changing children. */
+        std::pair<std::recursive_mutex, std::vector<std::shared_ptr<Node>>> mtxChildNodes;
+
+        /**
+         * Do not delete. Attached parent node. Using a raw pointer because it's a non-owning reference,
+         * and parent holds a shared_ptr to us. Should be used under the mutex when changing parent.
+         */
+        std::pair<std::recursive_mutex, Node*> mtxParentNode;
+
+        /**
+         * Whether this node is spawned in the world or not.
+         * Should be used under the mutex when spawning/despawning.
+         */
+        std::pair<std::recursive_mutex, bool> mtxIsSpawned;
+
+        /** Name of the category used for logging. */
+        static inline const auto sNodeLogCategory = "Node";
 
         ne_Node_GENERATED
     };

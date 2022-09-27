Flone-dnb/nameless-engine

commit 5145a69fc855f3e088e642cc2e6b5626cbd8818a

Basic functionality for `World` class.

src/engine_lib/private/game/nodes/Node.cpp
@@ -9,8 +9,6 @@ static std::atomic<size_t> iTotalNodeCount{0};
 #endif
 
 namespace ne {
-    Node::Node() : Node("Node") {}
-
     Node::Node(const std::string& sName) {
         this->sName = sName;
         mtxParentNode.second = nullptr;

src/engine_lib/public/game/nodes/Node.h
@@ -22,9 +22,6 @@ namespace ne NENAMESPACE() {
      */
     class NECLASS(Guid("2a721c37-3c22-450c-8dad-7b6985cbbd61")) Node : public Serializable {
     public:
-        /** Creates a node with the name "Node". */
-        Node();
-
         /**
          * Creates a node with the specified name.
          *
@@ -32,6 +29,7 @@ namespace ne NENAMESPACE() {
          */
         Node(const std::string& sNodeName);
 
+        Node() = delete;
         Node(const Node&) = delete;
         Node& operator=(const Node&) = delete;
         Node(Node&&) = delete;

src/engine_lib/CMakeLists.txt
@@ -49,6 +49,7 @@ if (NOT EXISTS ${GENERATED_REFLECTION_FILE_PATH})
 endif()
 # Engine general (cross-platform) sources.
 set(ENGINE_LIB_SOURCES
+    ../.generated/${REFLECTION_FILE_NAME}
     ${RELATIVE_EXT_PATH}/xxHash/xxhash.c
     ${RELATIVE_EXT_PATH}/xxHash/xxhash.h
     private/misc/Error.cpp
@@ -109,7 +110,8 @@ set(ENGINE_LIB_SOURCES
     private/io/DontSerializeProperty.cpp
     private/io/GuidProperty.cpp
     public/io/GuidProperty.h
-    ../.generated/${REFLECTION_FILE_NAME}
+    private/game/World.h
+    private/game/World.cpp
     # add general (cross-platform) sources here...
 )
 

src/engine_lib/private/game/World.cpp
@@ -1,0 +1,24 @@
+#include "World.h"
+
+// Custom.
+#include "misc/Error.h"
+
+namespace ne {
+
+    World::World(size_t iWorldSize, std::unique_ptr<Node> pRootNode) {
+        if (!std::has_single_bit(iWorldSize)) {
+            Error err(fmt::format(
+                "World size {} should be power of 2 (128, 256, 512, 1024, 2048, etc.).", iWorldSize));
+            err.showError();
+            throw std::runtime_error(err.getError());
+        }
+
+        this->iWorldSize = iWorldSize;
+        this->pRootNode = std::move(pRootNode);
+
+        timeWhenWorldCreated = std::chrono::steady_clock::now();
+    }
+
+    Node* World::getRootNode() const { return pRootNode.get(); }
+
+} // namespace ne

src/engine_lib/private/game/World.h
@@ -1,0 +1,71 @@
+#pragma once
+
+// STL.
+#include <memory>
+#include <chrono>
+#include <string>
+
+// Custom.
+#include "game/nodes/Node.h"
+
+namespace ne {
+    /** Owns world's root node. */
+    class World {
+    public:
+        World(const World&) = delete;
+        World& operator=(const World&) = delete;
+        World(World&&) = delete;
+        World& operator=(World&&) = delete;
+
+        /**
+         * Creates a new world with a root node to store world's node tree.
+         *
+         * @param sRootNodeName Name of the world's root node.
+         * @param iWorldSize    Size of the world in game units. Must be power of 2
+         * (128, 256, 512, 1024, 2048, etc.). World size needs to be specified for
+         * internal purposes such as Directional Light shadow map size, maybe for the size
+         * of correct physics calculations and etc. You don't need to care why we need this
+         * information, you only need to know that if you leave world bounds lighting
+         * or physics may be incorrect (the editor and logs should help you identify this case).
+         *
+         * @return New world instance.
+         */
+        template <typename RootNodeType>
+        requires std::derived_from<RootNodeType, Node>
+        static std::unique_ptr<World> createWorld(const std::string& sRootNodeName, size_t iWorldSize = 1024);
+
+        /**
+         * Returns a non owning pointer to world's root node.
+         *
+         * @return Do not delete returned pointer. World's root node.
+         */
+        Node* getRootNode() const;
+
+    private:
+        /**
+         * Creates a new world with the specified root node.
+         *
+         * @param iWorldSize World size in game units. Must be power of 2
+         * (128, 256, 512, 1024, 2048, etc.).
+         * @param pRootNode  World's root node.
+         */
+        World(size_t iWorldSize, std::unique_ptr<Node> pRootNode);
+
+        /** World's root node. */
+        std::unique_ptr<Node> pRootNode;
+
+        /** World size in game units. */
+        size_t iWorldSize = 0;
+
+        /** Time when world was created. */
+        std::chrono::steady_clock::time_point timeWhenWorldCreated;
+    };
+
+    template <typename RootNodeType>
+    requires std::derived_from<RootNodeType, Node> std::unique_ptr<World>
+    World::createWorld(const std::string& sRootNodeName, size_t iWorldSize) {
+        auto pRootNode = std::make_unique<RootNodeType>(sRootNodeName);
+
+        return std::unique_ptr<World>(new World(iWorldSize, std::move(pRootNode)));
+    }
+} // namespace ne

commit cd69be47d57424f23d0df991bd4740962951c3a4

Fix regression that was introduced in 5145a69.
In the development process I removed `Node`'s default constructor
which caused its deserialization to fail due to no default constructor.
There's actually nothing wrong with `Node` having a default constructor
right now so I returned it back.

src/engine_lib/private/game/nodes/Node.cpp
@@ -9,6 +9,8 @@ static std::atomic<size_t> iTotalNodeCount{0};
 #endif
 
 namespace ne {
+    Node::Node() : Node("Node") {}
+
     Node::Node(const std::string& sName) {
         this->sName = sName;
         mtxParentNode.second = nullptr;

src/engine_lib/public/game/nodes/Node.h
@@ -23,13 +23,17 @@ namespace ne NENAMESPACE() {
     class NECLASS(Guid("2a721c37-3c22-450c-8dad-7b6985cbbd61")) Node : public Serializable {
     public:
         /**
+         * Creates a node with a default name.
+         */
+        Node();
+
+        /**
          * Creates a node with the specified name.
          *
          * @param sNodeName Name of this node.
          */
         Node(const std::string& sNodeName);
 
-        Node() = delete;
         Node(const Node&) = delete;
         Node& operator=(const Node&) = delete;
         Node(Node&&) = delete;
61db59a007037e3d4d8e1dd77dafc63064903fcc
Alexander Tretyakov <flonednb@gmail.com>
2022-11-23 22:06:37 +0500

    Use plain GLM for math.

--- src/engine_lib/public/math/MathHelpers.hpp
+++ src/engine_lib/public/math/MathHelpers.hpp
@@ -3,38 +3,13 @@
 // Standard.
 #include <cmath>
 
-// Custom.
-#include "math/Vector.hpp"
-
 // External.
-#if defined(WIN32)
-#include "DirectXMath.h"
-#else
 #include "math/GLMath.hpp"
-#endif
 
 namespace ne {
     /** Static helper functions for math. */
     class MathHelpers {
         MathHelpers() = delete;
-
-        /**
-         * Converts radians to degrees.
-         *
-         * @param radians Radians to convert.
-         *
-         * @return Degrees.
-         */
-        static inline float convertRadiansToDegrees(float radians);
-
-        /**
-         * Converts degrees to radians.
-         *
-         * @param degrees Degrees to convert.
-         *
-         * @return Radians.
-         */
-        static inline float convertDegreesToRadians(float degrees);
 
         /**
          * Converts coordinates from the spherical coordinate system to the Cartesian coordinate system.
@@ -45,33 +20,12 @@ namespace ne {
          *
          * @return Coordinates in Cartesian coordinate system.
          */
-        static inline Vector convertSphericalToCartesianCoordinates(float radius, float theta, float phi);
+        static inline glm::vec3 convertSphericalToCartesianCoordinates(float radius, float theta, float phi);
     };
 
-    Vector MathHelpers::convertSphericalToCartesianCoordinates(float radius, float theta, float phi) {
-        Vector output;
-        output.setX(radius * sinf(phi) * cosf(theta));
-        output.setY(radius * sinf(phi) * sinf(theta));
-        output.setZ(radius * cosf(phi));
-
-        return output;
+    glm::vec3 MathHelpers::convertSphericalToCartesianCoordinates(float radius, float theta, float phi) {
+        return glm::vec3(
+            radius * sinf(phi) * cosf(theta), radius * sinf(phi) * sinf(theta), radius * cosf(phi));
+        ;
     }
-
-#if defined(WIN32)
-    // --------------------------------------------------------------------------------------------
-    // DirectXMath implementation.
-    // --------------------------------------------------------------------------------------------
-
-    float MathHelpers::convertRadiansToDegrees(float radians) { return DirectX::XMConvertToDegrees(radians); }
-
-    float MathHelpers::convertDegreesToRadians(float degrees) { return DirectX::XMConvertToRadians(degrees); }
-#else
-    // --------------------------------------------------------------------------------------------
-    // GLM implementation.
-    // --------------------------------------------------------------------------------------------
-
-    float MathHelpers::convertRadiansToDegrees(float radians) { return glm::degrees(radians); }
-
-    float MathHelpers::convertDegreesToRadians(float degrees) { return glm::radians(degrees); }
-#endif
 } // namespace ne

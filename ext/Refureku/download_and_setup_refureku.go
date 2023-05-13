f3f2cac5b3964558661bff2f8ef35b276acf6994
Aleksandr Tretyakov <flonednb@gmail.com>
2023-05-13 16:53:48 +0500

    Simplify reflection generator usage.

--- ext/Refureku/download_and_setup_refureku.go
+++ ext/Refureku/download_and_setup_refureku.go
@@ -17,25 +17,98 @@ import (
 	"github.com/pelletier/go-toml/v2"
 )
 
+var log_prefix = "download_and_setup_refureku.go:"
+
+type RefurekuSettings struct {
+	CodeGenManagerSettings struct {
+		SupportedFileExtensions []string `toml:"supportedFileExtensions"`
+		ToProcessDirectories    []string `toml:"toProcessDirectories"`
+		ToProcessFiles          []string `toml:"toProcessFiles"`
+		IgnoredDirectories      []string `toml:"ignoredDirectories"`
+		IgnoredFiles            []string `toml:"ignoredFiles"`
+	}
+	CodeGenUnitSettings struct {
+		OutputDirectory                string `toml:"outputDirectory"`
+		GeneratedHeaderFileNamePattern string `toml:"generatedHeaderFileNamePattern"`
+		GeneratedSourceFileNamePattern string `toml:"generatedSourceFileNamePattern"`
+		ClassFooterMacroPattern        string `toml:"classFooterMacroPattern"`
+		HeaderFileFooterMacroPattern   string `toml:"headerFileFooterMacroPattern"`
+	}
+	ParsingSettings struct {
+		CppVersion                            int      `toml:"cppVersion"`
+		ProjectIncludeDirectories             []string `toml:"projectIncludeDirectories"`
+		CompilerExeName                       string   `toml:"compilerExeName"`
+		AdditionalClangArguments              string   `toml:"additionalClangArguments"`
+		ShouldFailCodeGenerationOnClangErrors bool     `toml:"shouldFailCodeGenerationOnClangErrors"`
+		ShouldAbortParsingOnFirstError        bool     `toml:"shouldAbortParsingOnFirstError"`
+		ShouldParseAllNamespaces              bool     `toml:"shouldParseAllNamespaces"`
+		ShouldParseAllClasses                 bool     `toml:"shouldParseAllClasses"`
+		ShouldParseAllStructs                 bool     `toml:"shouldParseAllStructs"`
+		ShouldParseAllVariables               bool     `toml:"shouldParseAllVariables"`
+		ShouldParseAllFields                  bool     `toml:"shouldParseAllFields"`
+		ShouldParseAllFunctions               bool     `toml:"shouldParseAllFunctions"`
+		ShouldParseAllMethods                 bool     `toml:"shouldParseAllMethods"`
+		ShouldParseAllEnums                   bool     `toml:"shouldParseAllEnums"`
+		ShouldParseAllEnumValues              bool     `toml:"shouldParseAllEnumValues"`
+		ShouldLogDiagnostic                   bool     `toml:"shouldLogDiagnostic"`
+		PropertySeparator                     string   `toml:"propertySeparator"`
+		SubPropertySeparator                  string   `toml:"subPropertySeparator"`
+		SubPropertyStartEncloser              string   `toml:"subPropertyStartEncloser"`
+		SubPropertyEndEncloser                string   `toml:"subPropertyEndEncloser"`
+		NamespaceMacroName                    string   `toml:"namespaceMacroName"`
+		ClassMacroName                        string   `toml:"classMacroName"`
+		StructMacroName                       string   `toml:"structMacroName"`
+		VariableMacroName                     string   `toml:"variableMacroName"`
+		FieldMacroName                        string   `toml:"fieldMacroName"`
+		FunctionMacroName                     string   `toml:"functionMacroName"`
+		MethodMacroName                       string   `toml:"methodMacroName"`
+		EnumMacroName                         string   `toml:"enumMacroName"`
+		EnumValueMacroName                    string   `toml:"enumValueMacroName"`
+	}
+}
+
 // Expects the following arguments:
 // 1. Working directory (the directory where this script is located).
-// 2. Directory where source code is located.
-// 3. Array of included directories that the project uses separated by pipe ('|') sign.
-// 4. Array of files to exclude from analyzing.
-// 5. Used compiler ID (from CMake).
+// 2. Root directory where your source code is located (all subdirectories will be recursively scanned).
+// 3. Optional (can be empty). Absolute path to the directory with generated source code of the cmake target that's part
+// of the engine that your target depends on. For example if your `game_lib` target depends on `engine_lib` target then
+// you need to specify path to the "generated" directory of the `engine_lib`. This will result in all included directories,
+// of the target you depend on, to be appended to your specified included directories so that Refureku can find all includes.
+// 4. Array of included directories that the project uses separated by the pipe "|" character. This array must also include
+// all included directories of external dependencies (excluding engine-related targets).
+// 5. Array of files located in the source code directory (or subdirectory) to exclude from analyzing.
+// 6. Used compiler ID (from CMake).
 func main() {
 	var args_count = len(os.Args[1:])
-	if args_count < 5 {
-		fmt.Println("ERROR: download_and_setup_refureku.go: not enough arguments.")
+	if args_count < 6 {
+		fmt.Println(log_prefix, "not enough arguments.")
 		os.Exit(1)
 	}
 
 	// Save command line arguments.
 	var working_directory = os.Args[1]
 	var src_dir = os.Args[2]
-	var include_directories = strings.Split(os.Args[3], "|")
-	var exclude_files = strings.Split(os.Args[4], "|")
-	var compiler_id = os.Args[5]
+	var depends_generated_dir = os.Args[3]
+	var parsed_include_directories = strings.Split(os.Args[4], "|")
+	var exclude_files = strings.Split(os.Args[5], "|")
+	var compiler_id = os.Args[6]
+
+	// Remove empty elements from parsed includes
+	var include_directories = []string{}
+	for i := 0; i < len(parsed_include_directories); i++ {
+		if parsed_include_directories[i] != "" {
+			include_directories = append(include_directories, parsed_include_directories[i])
+		}
+	}
+
+	// Make sure all included directories exist.
+	for i := 0; i < len(include_directories); i++ {
+		var _, err = os.Stat(include_directories[i])
+		if os.IsNotExist(err) {
+			fmt.Println(log_prefix, "the specified included directory", include_directories[i], "does not exist")
+			os.Exit(1)
+		}
+	}
 
 	// Change this to update used Refureku version.
 	var refureku_version_tag = "v2.3.0e"
@@ -60,6 +133,7 @@ func main() {
 		src_dir,
 		include_directories,
 		exclude_files,
+		depends_generated_dir,
 		compiler_id)
 }
 
@@ -68,17 +142,25 @@ func get_archive_name(archive_url string) string {
 }
 
 func download_refureku_build(working_directory string, URL string, unzip_dir string) {
-	var archive_path = filepath.Join(working_directory, get_archive_name(URL))
+	// Check that working directory path exists.
+	var _, err = os.Stat(working_directory)
+	if os.IsNotExist(err) {
+		fmt.Println(log_prefix, "the specified path to the script file",
+			working_directory, "does not exist")
+		os.Exit(1)
+	}
 
-	var _, err = os.Stat(archive_path)
+	// See if up to date build is already downloaded and exit early.
+	var archive_path = filepath.Join(working_directory, get_archive_name(URL))
+	_, err = os.Stat(archive_path)
 	if err == nil {
 		// Exists.
-		fmt.Println("INFO: download_and_setup_refureku.go: found already downloaded Refureku build at",
+		fmt.Println(log_prefix, "found already downloaded Refureku build at",
 			archive_path, "- nothing to download")
 		return
 	}
 
-	// Not found. See if there are any .zip/tar.gz files and remove them.
+	// No build found. See if there are any .zip/tar.gz files and remove them.
 	items, _ := ioutil.ReadDir(working_directory)
 	for _, item := range items {
 		if item.IsDir() {
@@ -90,30 +172,30 @@ func download_refureku_build(working_directory string, URL string, unzip_dir str
 		}
 	}
 
-	fmt.Println("INFO: download_and_setup_refureku.go: downloading archived build to", archive_path)
+	fmt.Println(log_prefix, "downloading archived build to", archive_path)
 
 	response, err := http.Get(URL)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go:", err)
+		fmt.Println(log_prefix, err)
 		os.Exit(1)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		fmt.Println("ERROR: download_and_setup_refureku.go: received non 200 response code, actual result:", response.StatusCode)
+		fmt.Println(log_prefix, "received non 200 response code, actual result:", response.StatusCode)
 		os.Exit(1)
 	}
 
 	file, err := os.Create(archive_path)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go: failed to create empty file, error:", err)
+		fmt.Println(log_prefix, "failed to create empty file, error:", err)
 		os.Exit(1)
 	}
 	defer file.Close()
 
 	_, err = io.Copy(file, response.Body)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go: failed to copy downloaded bytes, error:", err)
+		fmt.Println(log_prefix, "failed to copy downloaded bytes, error:", err)
 		os.Exit(1)
 	}
 
@@ -128,7 +210,7 @@ func remove_old_refureku_build(working_directory string) {
 		// Exists.
 		err = os.RemoveAll(current_path)
 		if err != nil {
-			fmt.Println("ERROR: download_and_setup_refureku.go: failed to remove old Refureku build, error:", err)
+			fmt.Println(log_prefix, "failed to remove old Refureku build, error:", err)
 			os.Exit(1)
 		}
 	}
@@ -137,7 +219,7 @@ func remove_old_refureku_build(working_directory string) {
 func unzip(src string, dest string) {
 	var archive, err = os.Open(src)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go: failed to read archive file, error:", err)
+		fmt.Println(log_prefix, "failed to read archive file, error:", err)
 		os.Exit(1)
 	}
 
@@ -148,7 +230,7 @@ func unzip(src string, dest string) {
 		ctx := context.TODO()
 		extract.Gz(ctx, archive, dest, nil)
 	} else {
-		fmt.Println("ERROR: download_and_setup_refureku.go: unknown archive extension", src)
+		fmt.Println(log_prefix, "unknown archive extension", src)
 		os.Exit(1)
 	}
 
@@ -161,7 +243,7 @@ func unzip(src string, dest string) {
 		var refureku_generator_path = filepath.Join(dest, "Bin", "RefurekuGenerator")
 		_, err = os.Stat(refureku_generator_path)
 		if os.IsNotExist(err) {
-			fmt.Println("ERROR: download_and_setup_refureku.go: Refureku generator does not exist at",
+			fmt.Println(log_prefix, "Refureku generator does not exist at",
 				refureku_generator_path)
 			os.Exit(1)
 		}
@@ -169,7 +251,7 @@ func unzip(src string, dest string) {
 		// Allow executing the generator.
 		var err = session.Command("chmod", "+x", refureku_generator_path).Run()
 		if err != nil {
-			fmt.Println("ERROR: download_and_setup_refureku.go: failed to add 'execute' permission on file",
+			fmt.Println(log_prefix, "failed to add 'execute' permission on file",
 				refureku_generator_path)
 			os.Exit(1)
 		}
@@ -181,78 +263,46 @@ func initialize_refureku_settings(
 	src_directory string,
 	include_directories []string,
 	exclude_files []string,
+	depends_generated_dir string,
 	compiler_id string) {
 	// Check that template file exists.
 	var _, err = os.Stat(template_settings_file_path)
 	if os.IsNotExist(err) {
-		fmt.Println("ERROR: download_and_setup_refureku.go: the specified template Refureku settings file",
+		fmt.Println(log_prefix, "the specified template Refureku settings file",
 			template_settings_file_path, "does not exist")
 		os.Exit(1)
 	}
 
-	// Check that output directory exists
-	_, err = os.Stat(template_settings_file_path)
+	// Check that directory with source code exists.
+	_, err = os.Stat(src_directory)
 	if os.IsNotExist(err) {
-		fmt.Println("ERROR: download_and_setup_refureku.go: the specified output directory",
+		fmt.Println(log_prefix, "the specified source files directory",
 			src_directory, "does not exist")
 		os.Exit(1)
 	}
 
-	var out_settings_file = filepath.Join(src_directory, ".generated", "RefurekuSettings.toml")
+	var refureku_config_file_name = "RefurekuSettings.toml"
+
+	if depends_generated_dir != "" {
+		// Append includes of depends target.
+		var depends_settings_file = filepath.Join(depends_generated_dir, refureku_config_file_name)
+		_, err = os.Stat(depends_settings_file)
+		if os.IsNotExist(err) {
+			fmt.Println(log_prefix, "the specified Refureku config file of the directory you depend on",
+				depends_settings_file, "does not exist")
+			os.Exit(1)
+		}
+		include_directories = append(include_directories, get_included_directories_of_depends_target(depends_settings_file)[:]...)
+	}
+
+	var out_settings_file = filepath.Join(src_directory, ".generated", refureku_config_file_name)
+
 	// Don't check if settings file already exists to skip this step.
 	// Otherwrite with new settings (maybe this script changed due to git updates).
 
-	type RefurekuSettings struct {
-		CodeGenManagerSettings struct {
-			SupportedFileExtensions []string `toml:"supportedFileExtensions"`
-			ToProcessDirectories    []string `toml:"toProcessDirectories"`
-			ToProcessFiles          []string `toml:"toProcessFiles"`
-			IgnoredDirectories      []string `toml:"ignoredDirectories"`
-			IgnoredFiles            []string `toml:"ignoredFiles"`
-		}
-		CodeGenUnitSettings struct {
-			OutputDirectory                string `toml:"outputDirectory"`
-			GeneratedHeaderFileNamePattern string `toml:"generatedHeaderFileNamePattern"`
-			GeneratedSourceFileNamePattern string `toml:"generatedSourceFileNamePattern"`
-			ClassFooterMacroPattern        string `toml:"classFooterMacroPattern"`
-			HeaderFileFooterMacroPattern   string `toml:"headerFileFooterMacroPattern"`
-		}
-		ParsingSettings struct {
-			CppVersion                            int      `toml:"cppVersion"`
-			ProjectIncludeDirectories             []string `toml:"projectIncludeDirectories"`
-			CompilerExeName                       string   `toml:"compilerExeName"`
-			AdditionalClangArguments              string   `toml:"additionalClangArguments"`
-			ShouldFailCodeGenerationOnClangErrors bool     `toml:"shouldFailCodeGenerationOnClangErrors"`
-			ShouldAbortParsingOnFirstError        bool     `toml:"shouldAbortParsingOnFirstError"`
-			ShouldParseAllNamespaces              bool     `toml:"shouldParseAllNamespaces"`
-			ShouldParseAllClasses                 bool     `toml:"shouldParseAllClasses"`
-			ShouldParseAllStructs                 bool     `toml:"shouldParseAllStructs"`
-			ShouldParseAllVariables               bool     `toml:"shouldParseAllVariables"`
-			ShouldParseAllFields                  bool     `toml:"shouldParseAllFields"`
-			ShouldParseAllFunctions               bool     `toml:"shouldParseAllFunctions"`
-			ShouldParseAllMethods                 bool     `toml:"shouldParseAllMethods"`
-			ShouldParseAllEnums                   bool     `toml:"shouldParseAllEnums"`
-			ShouldParseAllEnumValues              bool     `toml:"shouldParseAllEnumValues"`
-			ShouldLogDiagnostic                   bool     `toml:"shouldLogDiagnostic"`
-			PropertySeparator                     string   `toml:"propertySeparator"`
-			SubPropertySeparator                  string   `toml:"subPropertySeparator"`
-			SubPropertyStartEncloser              string   `toml:"subPropertyStartEncloser"`
-			SubPropertyEndEncloser                string   `toml:"subPropertyEndEncloser"`
-			NamespaceMacroName                    string   `toml:"namespaceMacroName"`
-			ClassMacroName                        string   `toml:"classMacroName"`
-			StructMacroName                       string   `toml:"structMacroName"`
-			VariableMacroName                     string   `toml:"variableMacroName"`
-			FieldMacroName                        string   `toml:"fieldMacroName"`
-			FunctionMacroName                     string   `toml:"functionMacroName"`
-			MethodMacroName                       string   `toml:"methodMacroName"`
-			EnumMacroName                         string   `toml:"enumMacroName"`
-			EnumValueMacroName                    string   `toml:"enumValueMacroName"`
-		}
-	}
-
 	in_file, err := os.Open(template_settings_file_path)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go: failed to open Refureku template "+
+		fmt.Println(log_prefix, "failed to open Refureku template "+
 			"settings file:", err)
 		os.Exit(1)
 	}
@@ -265,10 +315,10 @@ func initialize_refureku_settings(
 	if err != nil {
 		var details *toml.StrictMissingError
 		if errors.As(err, &details) {
-			fmt.Println("ERROR: download_and_setup_refureku.go: error while reading Refureku template "+
+			fmt.Println(log_prefix, "error while reading Refureku template "+
 				"settings file - StrictMissingError:", details.String())
 		} else {
-			fmt.Println("ERROR: download_and_setup_refureku.go: error while reading Refureku template "+
+			fmt.Println(log_prefix, "error while reading Refureku template "+
 				"settings file:", err)
 		}
 
@@ -305,20 +355,49 @@ func initialize_refureku_settings(
 	// Save new configuration.
 	out_file, err := os.Create(out_settings_file)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go: failed to create new Refureku settings "+
+		fmt.Println(log_prefix, "failed to create new Refureku settings "+
 			"file:", err)
 		os.Exit(1)
 	}
 	bytes, err := toml.Marshal(cfg)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go:", err)
+		fmt.Println(log_prefix, err)
 		os.Exit(1)
 	}
 	_, err = out_file.Write(bytes)
 	if err != nil {
-		fmt.Println("ERROR: download_and_setup_refureku.go: failed to write to new Refureku settings "+
+		fmt.Println(log_prefix, "failed to write to new Refureku settings "+
 			"file:", err)
 		os.Exit(1)
 	}
 	out_file.Close()
+}
+
+func get_included_directories_of_depends_target(path_to_refureku_settings string) []string {
+	in_file, err := os.Open(path_to_refureku_settings)
+	if err != nil {
+		fmt.Println(log_prefix, "failed to open Refureku "+
+			"settings file:", err)
+		os.Exit(1)
+	}
+
+	d := toml.NewDecoder(in_file)
+	d.DisallowUnknownFields()
+
+	var cfg RefurekuSettings
+	err = d.Decode(&cfg)
+	if err != nil {
+		var details *toml.StrictMissingError
+		if errors.As(err, &details) {
+			fmt.Println(log_prefix, "error while reading Refureku "+
+				"settings file - StrictMissingError:", details.String())
+		} else {
+			fmt.Println(log_prefix, "error while reading Refureku "+
+				"settings file:", err)
+		}
+
+		os.Exit(1)
+	}
+
+	return cfg.ParsingSettings.ProjectIncludeDirectories
 }
--- src/.cmake/essential.cmake
+++ src/.cmake/essential.cmake
@@ -36,3 +36,153 @@ set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 # Define some folder names.
 set(ENGINE_FOLDER "NAMELESS-ENGINE")
 set(EXTERNAL_FOLDER "EXTERNAL")
+
+# Refureku related stuff: ------------------------------------------------------------------------------------
+set(GENERATED_DIR_NAME ".generated")
+set(REFUREKU_SETTINGS_FILE_NAME "RefurekuSettings.toml")
+
+# Adds Refureku (reflection generator) as a PRE_BUILD dependency to your `${PROJECT_NAME}` target.
+#
+# Arguments:
+# - TARGET_CMAKE_DIR                - value of ${CMAKE_CURRENT_SOURCE_DIR}. This directory is assumed to also be the root directory
+# for your source code, all files (from all subdirectories) will be recursively analyzed by the reflection generator.
+# - EXCLUDED_FROM_ANALYZING         - a list of files that are located in the directory with the source code but should be excluded
+# from analyzing, this is typically used for platform specific source files, for example if you are building on Linux
+# you want to exclude Win32 sources.
+# - REFUREKU_EXT_PATH               - absolute path to the directory `ext/Refureku`.
+# - ENGINE_DEPENDENCY_TARGET        - optional (can be empty), if your target depends on some other engine-related target
+# (for example if your `game_lib` target depends on the `engine_lib` target), specify name of the engine-related target
+# you depend on. This will result in all specified Refureku included directories, of the target you depend on, to be
+# appended to your specified included directories so that Refureku can find all includes.
+# - INCLUDED_DIRECTORIES            - a list of all included directories (including `include`s of all other
+# non-engine related dependencies (if you, for example, depend on `engine_lib` then just specify path to its ".generated"
+# directory as other argument of this function instead of writing all includes of `engine_lib`)),
+# this list typically contains a list of paths to "include" directories of external dependencies plus path to
+# included directories of your target.
+function(add_refureku TARGET_CMAKE_DIR EXCLUDED_FROM_ANALYZING REFUREKU_EXT_PATH ENGINE_DEPENDENCY_TARGET INCLUDED_DIRECTORIES)
+    message(STATUS "${PROJECT_NAME}: adding external dependency \"Refureku\" (reflection generator)...")
+
+    # Make sure the path to the Refureku directory exists.
+    if (NOT EXISTS ${REFUREKU_EXT_PATH})
+         message(FATAL_ERROR "${PROJECT_NAME}: path to Refureku directory ${REFUREKU_EXT_PATH} does not exist.")
+    endif()
+
+    # Make sure the script path exists.
+    set(SCRIPT_PATH ${REFUREKU_EXT_PATH}/download_and_setup_refureku.go)
+    if (NOT EXISTS ${SCRIPT_PATH})
+        message(FATAL_ERROR "${PROJECT_NAME}: path to the script file ${SCRIPT_PATH} does not exist.")
+    endif()
+
+    # Construct path to the ".generated" directory of the target we depend on.
+    set(ENGINE_DEPENDENCY_GENERATED_DIR "")
+    if (NOT "${ENGINE_DEPENDENCY_TARGET}" STREQUAL "")
+        set(ENGINE_DEPENDENCY_GENERATED_DIR ${TARGET_CMAKE_DIR}/../${ENGINE_DEPENDENCY_TARGET}/${GENERATED_DIR_NAME})
+        if (NOT EXISTS ${ENGINE_DEPENDENCY_GENERATED_DIR})
+            message(FATAL_ERROR "${PROJECT_NAME}: expected the path ${ENGINE_DEPENDENCY_GENERATED_DIR} to the \".generated\" directory of the target we depend on to exist")
+        endif()
+    endif()
+
+    # Append ".generated" directory to the include paths.
+    set(INCLUDED_DIRECTORIES
+        ${INCLUDED_DIRECTORIES}
+        ${TARGET_CMAKE_DIR}/${GENERATED_DIR_NAME}
+    )
+
+    # Define some paths.
+    set(GENERATED_SOURCE_DIR_PATH ${TARGET_CMAKE_DIR}/${GENERATED_DIR_NAME})
+    set(REFUREKU_SETTINGS_FILE_PATH ${GENERATED_SOURCE_DIR_PATH}/${REFUREKU_SETTINGS_FILE_NAME})
+
+    # Check that path to target's cmake file does not have pipe characters in it because we will use pipes to
+    # separate include paths later.
+    string(FIND ${TARGET_CMAKE_DIR} "|" PIPE_POS)
+    if (NOT ${PIPE_POS} STREQUAL "-1")
+        message(FATAL_ERROR "${PROJECT_NAME}: path ${TARGET_CMAKE_DIR} contains the character `|` which is used for internal purposes.")
+    endif()
+
+    # Merge included directories into one string while separating each filename with the pipe character "|".
+    set(REFUREKU_INCLUDE_DIRECTORIES "")
+    foreach(DIR_PATH IN LISTS INCLUDED_DIRECTORIES)
+        string(FIND ${DIR_PATH} "|" PIPE_POS)
+        if (NOT ${PIPE_POS} STREQUAL "-1")
+            message(FATAL_ERROR "${PROJECT_NAME}: included directory path ${DIR_PATH} contains the character `|` which is used for internal purposes.")
+        endif()
+        set(REFUREKU_INCLUDE_DIRECTORIES ${DIR_PATH}|${REFUREKU_INCLUDE_DIRECTORIES})
+    endforeach()
+
+    # Merge excluded files into one string while separating each filename with the pipe character "|".
+    set(REFUREKU_EXCLUDE_FILES "")
+    foreach(SRC_PATH IN LISTS EXCLUDED_FROM_ANALYZING)
+        string(FIND ${SRC_PATH} "|" PIPE_POS)
+        if (NOT ${PIPE_POS} STREQUAL "-1")
+            message(FATAL_ERROR "${PROJECT_NAME}: excluded file path ${SRC_PATH} contains the character `|` which is used for internal purposes.")
+        endif()
+        set(REFUREKU_EXCLUDE_FILES ${SRC_PATH}|${REFUREKU_EXCLUDE_FILES})
+    endforeach()
+
+    # Create the generated directory if not exists.
+    if (NOT EXISTS ${GENERATED_SOURCE_DIR_PATH})
+        file(MAKE_DIRECTORY ${GENERATED_SOURCE_DIR_PATH})
+    endif()
+
+    # Delete existing Refureku config file.
+    message(STATUS "${PROJECT_NAME}: removing Refureku (reflection generator) config file to generate a fresh one...")
+    file(REMOVE ${REFUREKU_SETTINGS_FILE_PATH})
+
+    # Download Refureku and generate new config file.
+    message(STATUS "${PROJECT_NAME}: downloading Refureku (reflection generator) and creating a new reflection generator config...")
+    execute_process(
+        WORKING_DIRECTORY ${REFUREKU_EXT_PATH}
+        COMMAND go run ${SCRIPT_PATH}
+        ${REFUREKU_EXT_PATH} # directory where the script is located
+        ${TARGET_CMAKE_DIR}/ # directory where the source code is located
+        "${ENGINE_DEPENDENCY_GENERATED_DIR}" # depends ".generated" dir
+        "${REFUREKU_INCLUDE_DIRECTORIES}" # project include directories
+        "${REFUREKU_EXCLUDE_FILES}" # files to exclude from analyzing
+        ${CMAKE_CXX_COMPILER_ID} # used compiler
+        OUTPUT_VARIABLE refureku_output
+        ERROR_VARIABLE refureku_error
+    )
+    message(STATUS "${PROJECT_NAME}: ${refureku_output}")
+    if (refureku_error)
+        message(FATAL_ERROR "${refureku_error}")
+    endif()
+
+    # Include Refureku header files.
+    target_include_directories(${PROJECT_NAME} SYSTEM PUBLIC ${REFUREKU_EXT_PATH}/build/Include/)
+
+    # Link Refureku library.
+    if (MSVC)
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /ignore:4006")
+        set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /ignore:4006")
+        set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} /ignore:4006")
+    endif()
+    if (WIN32)
+        target_link_directories(${PROJECT_NAME} PUBLIC ${REFUREKU_EXT_PATH}/build/Lib)
+    elseif(APPLE) # test macos before unix
+        message(FATAL_ERROR "${PROJECT_NAME}: TODO: put Refureku dynamic library directory path here.")
+    elseif(UNIX)
+        target_link_directories(${PROJECT_NAME} PUBLIC ${REFUREKU_EXT_PATH}/build/Bin)
+    endif()
+    target_link_libraries(${PROJECT_NAME} PUBLIC Refureku)
+
+    # Add a pre build step to run reflection code generator.
+    set(REFUREKU_GENERATOR_TARGET ${PROJECT_NAME}_run_reflection_generator)
+    add_custom_target(${REFUREKU_GENERATOR_TARGET}
+        WORKING_DIRECTORY ${REFUREKU_EXT_PATH}/build/Bin
+        COMMAND ${REFUREKU_EXT_PATH}/build/Bin/RefurekuGenerator ${REFUREKU_SETTINGS_FILE_PATH}
+        ENVIRONMENT LD_LIBRARY_PATH ${REFUREKU_EXT_PATH}/build/Bin
+        COMMENT "${PROJECT_NAME}: running reflection generator..."
+    )
+    add_dependencies(${PROJECT_NAME} ${REFUREKU_GENERATOR_TARGET})
+    set_target_properties(${REFUREKU_GENERATOR_TARGET} PROPERTIES FOLDER ${EXTERNAL_FOLDER})
+
+    if (NOT "${ENGINE_DEPENDENCY_TARGET}" STREQUAL "")
+        # Make sure our reflection generator will only run after the target we depend on.
+        add_dependencies(${REFUREKU_GENERATOR_TARGET} ${ENGINE_DEPENDENCY_TARGET})
+    endif()
+
+    # Add generated headers to include directories.
+    target_include_directories(${PROJECT_NAME} SYSTEM PUBLIC ${GENERATED_SOURCE_DIR_PATH})
+endfunction()
+
+# ------------------------------------------------------------------------------------------------------------------

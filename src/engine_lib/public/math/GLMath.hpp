03263476cda624b8fc5865297aa39bf521ca6a17
Aleksandr Tretyakov <flonednb@gmail.com>
2023-10-20 12:41:22 +0500

    Write about additional reasons to not enable GLM padding.

--- src/engine_lib/public/math/GLMath.hpp
+++ src/engine_lib/public/math/GLMath.hpp
@@ -10,10 +10,12 @@
 #define GLM_FORCE_INLINE
 #define GLM_FORCE_XYZW_ONLY
 #define GLM_ENABLE_EXPERIMENTAL
-// #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES  // disabling these because they have proved to cause
-// #define GLM_FORCE_INTRINSICS                // some absurd crashed only in release builds
-// which are probably caused by me not following some rules for aligned types but in order to make
-// the life easier I'm disabling them
+// #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES  // Disabling these because they have proved to cause
+// #define GLM_FORCE_INTRINSICS                // some absurd crashed only in release builds which are
+// probably caused by me not following some rules for aligned types but in order to make the life easier I'm
+// disabling them. In addition, we also want to avoid using these macros to avoid bloating the data
+// we are passing to shaders and since we specify `alignas` on all fields of types that will be copied
+// to the GPU we expect no auto-padding and have a fine control of the alignment.
 
 #include "glm/glm.hpp"
 #include "glm/trigonometric.hpp"

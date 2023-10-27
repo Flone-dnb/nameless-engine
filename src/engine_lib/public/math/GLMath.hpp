a4bfd699d73048b2429e2daac470655bc51bac78
Aleksandr Tretyakov <flonednb@gmail.com>
2023-10-27 21:29:16 +0500

    Simplify docs.

--- src/engine_lib/public/math/GLMath.hpp
+++ src/engine_lib/public/math/GLMath.hpp
@@ -11,11 +11,7 @@
 #define GLM_FORCE_XYZW_ONLY
 #define GLM_ENABLE_EXPERIMENTAL
 // #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES  // Disabling these because they have proved to cause
-// #define GLM_FORCE_INTRINSICS                // some absurd crashed only in release builds which are
-// probably caused by me not following some rules for aligned types but in order to make the life easier I'm
-// disabling them. In addition, we also want to avoid using these macros to avoid bloating the data
-// we are passing to shaders and since we specify `alignas` on all fields of types that will be copied
-// to the GPU we expect no auto-padding and have a fine control of the alignment.
+// #define GLM_FORCE_INTRINSICS                // some absurd crashed only in release builds.
 
 #include "glm/glm.hpp"
 #include "glm/trigonometric.hpp"

3028046f3464a107c26cd55a9c6812181d697d9c
Aleksandr Tretyakov <flonednb@gmail.com>
2023-11-17 15:25:14 +0500

    Add more shapes in shaders.

--- res/engine/shaders/include/Shapes.glsl
+++ res/engine/shaders/include/Shapes.glsl
@@ -9,6 +9,29 @@ struct Plane{
     float distanceFromOrigin;
 };
 
+/** Sphere represented by a center and a radius. */
+struct Sphere{
+    /** Location of the sphere's center point. */
+    vec3 center;
+
+    /** Sphere's radius. */
+    float radius;
+};
+
+struct Cone{
+    /** Location of cone's tip. */
+    vec3 location;
+
+    /** Height of the cone. */
+    float height;
+
+    /** Direction unit vector from cone's tip. */
+    vec3 direction;
+
+    /** Radius of the bottom part of the cone. */
+    float radius;
+};
+
 /** Frustum in view space. */
 struct Frustum{
     /** Left, right, top and bottom faces of the frustum. */
@@ -41,4 +64,117 @@ Plane calculatePlaneFromTriangle(vec3 point0, vec3 point1, vec3 point2){
     plane.distanceFromOrigin = dot(plane.normal, point0);
 
     return plane;
+}
+
+/**
+ * Tells if the sphere is fully behind (inside the negative halfspace of) a plane.
+ *
+ * @param sphere Sphere to test.
+ * @param plane  Plane to test.
+ *
+ * @return `true` if the sphere is fully behind the plane, `false` otherwise.
+ */
+bool isSphereBehindPlane(Sphere sphere, Plane plane){
+    // Source: Real-time collision detection, Christer Ericson (2005).
+    return dot(plane.normal, sphere.center) - plane.distanceFromOrigin < -sphere.radius;
+}
+
+/**
+ * Tells if the point is fully behind (inside the negative halfspace of) a plane.
+ *
+ * @param pointToTest Point to test.
+ * @param plane       Plane to test.
+ *
+ * @return `true` if the point is fully behind the plane, `false` otherwise.
+ */
+bool isPointBehindPlane(vec3 pointToTest, Plane plane){ // apparently `point` is a keyword in HLSL so we can't use that name
+    // Source: Real-time collision detection, Christer Ericson (2005).
+    return dot(plane.normal, pointToTest) - plane.distanceFromOrigin < 0.0F;
+}
+
+/**
+ * Tells if the cone is fully behind (inside the negative halfspace of) a plane.
+ *
+ * @param cone  Cone to test.
+ * @param plane Plane to test.
+ *
+ * @return `true` if the cone is fully behind the plane, `false` otherwise.
+ */
+bool isConeBehindPlane(Cone cone, Plane plane){
+    // Source: Real-time collision detection, Christer Ericson (2005).
+
+    // Calculate an intermediate vector which is parallel but opposite to plane's normal and perpendicular
+    // to the cone's direction.
+    vec3 m = cross(cross(plane.normal, cone.direction), cone.direction);
+
+    // Calculate the point Q that is on the base (bottom) of the cone that is farthest away from the plane
+    // in the direction of plane's normal.
+    vec3 Q = cone.location + cone.direction * cone.height - m * cone.radius;
+
+    // The cone is behind the plane if both cone's tip and Q are behind the plane.
+    return isPointBehindPlane(cone.location, plane) && isPointBehindPlane(Q, plane);
+}
+
+/**
+ * Tells if the specified sphere is fully inside of the specified frustum or partially contained within the frustum.
+ *
+ * @remark Expects that far Z points in the positive Z direction, in other words: zNear < zFar.
+ *
+ * @param sphere  Sphere to test.
+ * @param frustum Frustum to test.
+ */
+bool isSphereInsideFrustum(Sphere sphere, Frustum frustum, float frustumZNear, float frustumZFar){
+    // Prepare output variable.
+    bool bIsInside = true;
+
+    // Test sphere against frustum near/far clip planes.
+    if (sphere.center.z - sphere.radius > frustumZFar || sphere.center.z + sphere.radius < frustumZNear){
+        bIsInside = false;
+    }
+
+    // Test sphere agains frustum planes.
+    for (int i = 0; i < 4 && bIsInside; i++){
+        if (isSphereBehindPlane(sphere, frustum.planes[i])){
+            bIsInside = false;
+        }
+    }
+
+    return bIsInside;
+}
+
+/**
+ * Tells if the specified cone is fully inside of the specified frustum or partially contained within the frustum.
+ *
+ * @remark Expects that far Z points in the positive Z direction, in other words: zNear < zFar.
+ *
+ * @param cone    Cone to test.
+ * @param frustum Frustum to test.
+ */
+bool isConeInsideFrustum(Cone cone, Frustum frustum, float frustumZNear, float frustumZFar){
+    // Prepare output variable.
+    bool bIsInside = true;
+
+    // Construct frustum near plane.
+    Plane frustumNearPlane;
+    frustumNearPlane.normal = vec3(0.0F, 0.0F, 1.0F);
+    frustumNearPlane.distanceFromOrigin = frustumZNear;
+
+    // Construct frustum far plane.
+    Plane frustumFarPlane;
+    frustumFarPlane.normal = vec3(0.0F, 0.0F, -1.0F);
+    frustumFarPlane.distanceFromOrigin = frustumZFar;
+
+    // Test cone against frustum near/far clip planes.
+    if (isConeBehindPlane(cone, frustumNearPlane) || isConeBehindPlane(cone, frustumFarPlane)){
+        bIsInside = false;
+    }
+
+    // Test sphere agains frustum planes.
+    for (int i = 0; i < 4 && bIsInside; i++){
+        if (isConeBehindPlane(cone, frustum.planes[i])){
+            bIsInside = false;
+        }
+    }
+
+    return bIsInside;
 }

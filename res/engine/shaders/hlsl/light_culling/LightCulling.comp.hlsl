8f278f786bc6672903ae86a37238c16fdbcbd515
Aleksandr Tretyakov <flonednb@gmail.com>
2023-11-29 13:30:26 +0500

    Avoid reading depth out of bounds.

--- res/engine/shaders/hlsl/light_culling/LightCulling.comp.hlsl
+++ res/engine/shaders/hlsl/light_culling/LightCulling.comp.hlsl
@@ -11,8 +11,16 @@ _Static_assert(false, "thread count in group - macro not defined");
 /** 1 thread per pixel in a tile. 1 thread group per tile. */
 [numthreads(LIGHT_GRID_TILE_SIZE_IN_PIXELS, LIGHT_GRID_TILE_SIZE_IN_PIXELS, 1 )]
 void csLightCulling(uint3 threadIdInDispatch : SV_DispatchThreadID, uint iThreadIdInGroup : SV_GroupIndex, uint3 groupIdInDispatch : SV_GroupID){
-    // Get depth of this pixel.
-    float depth = depthTexture.Load(int3(threadIdInDispatch.xy, 0)).r;
+    // Prepare variable to store depth.
+    float depth = 1.0F;
+
+    // Avoid reading depth out of bounds because if width/height is not evenly divisible by the thread group size
+    // we will `ceil` the result and dispatch slightly more threads. So some thread groups can have some threads
+    // that will be out of bounds.
+    if (threadIdInDispatch.x < screenToViewData.iRenderTargetWidth && threadIdInDispatch.y < screenToViewData.iRenderTargetHeight){
+        // Get depth of this pixel.
+        depth = depthTexture.Load(int3(threadIdInDispatch.xy, 0)).r;
+    }
 
     // Cull lights.
     cullLightsForTile(depth, groupIdInDispatch.x, groupIdInDispatch.y, iThreadIdInGroup);

3dd161793ab0428850309092ad6a26c97046bcae
Aleksandr Tretyakov <flonednb@gmail.com>
2024-05-15 19:51:37 +0300

    Update dependencies.

--- ext/DirectXShaderCompiler/download_dxc.go
+++ ext/DirectXShaderCompiler/download_dxc.go
@@ -21,7 +21,7 @@ func main() {
 	}
 
 	var working_directory = os.Args[1]
-	var archive_url = "https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2403.1/dxc_2024_03_22.zip"
+	var archive_url = "https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2403.2/dxc_2024_03_29.zip"
 
 	download_dxc_build(working_directory, archive_url)
 	remove_old_dxc_build(working_directory)
